10ecafde00dc71597c5814d34cc0e2cca98a532d
Mikas <mikas1213@gmail.com>
2025-09-03 11:12:00 +0300

    check background coor 1 [dev]

--- src/layouts/ClientLayout.tsx
+++ src/layouts/ClientLayout.tsx
@@ -2,15 +2,15 @@ import { Outlet } from 'react-router';
 
 const ClientLayout = () => {
 	return (
-		// <div className='container'>
-			// <div className='preventBounce'>
-            <>  
+		<div className='container'>
+			
+            
 				<h3>Header</h3>
 				<Outlet />
 				<h4>Footer</h4>
-            </>
-			// </div>
-		// </div>
+            
+			
+		</div>
 	);
 };
 
